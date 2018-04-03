7c2cf87749bbe5e98dadbe3ad0b7de411e7aebfd
qiyanpeng <qiyanpeng@sogou-inc.com>
2018-04-03 19:53:35 +0800

    修改了启动server和关闭server的方法

--- .ws/alias.sh
+++ .ws/alias.sh
@@ -14,11 +14,24 @@ function to_dir {
         cd $TOP_ROOT/docs/$1
     fi
 }
-
+PID_FILE='/tmp/mkdocs_demon_lock.pid'
 function run_server {
+    if [ -f "$PID_FILE" ]; then
+        echo 'A mkdocs has already running...'
+        return
+    fi
     cd $TOP_ROOT
-    mkdocs serve -a 0.0.0.0:8000
+    nohup mkdocs serve -a 0.0.0.0:8000 2>&1 & 
+    echo $! > $PID_FILE
+}
+
+function kill_server {
+     if [ -f "$PID_FILE" ]; then
+        kill -9 `cat $PID_FILE`
+        rm -f $PID_FILE
+    fi  
 }
 
 alias to='to_dir '
 alias run='run_server'
+alias kl='kill_server'
